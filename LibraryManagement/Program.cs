3c8922e6f03db7cd07019189b5ac74c782a4f66d
UwUssimo Robinson <uwussimo@icloud.com>
2021-12-17 14:26:21 +0500

    uploading uml and client changes

--- LibraryManagement/Program.cs
+++ LibraryManagement/Program.cs
@@ -3,19 +3,27 @@
 using System;
 using LibraryManagement;
 
-// If we use Atham who is admin, we can use any method as we are admin
-// If we use something else, we can't use any method which are not accessible for non-admins
-//var admin  = new Client("Atham");
+namespace LibraryManagement;
 
-//Console.WriteLine("From admin perspective");
-//admin.GetBooks();
-//admin.AddUser("Alan");
-//admin.AddBook("Hello World", "Alan", "2f23f2323f23f");
-//admin.GetBooks();
+public class Program
+{
+    public static void Main()
+    {
+        // If we use Atham who is admin, we can use any method as we are admin
+        // If we use something else, we can't use any method which are not accessible for non-admins
+        //var admin  = new Client("Atham");
 
-// As there is no any database to save all data,
-// any changes made on various clients won't be saved
-//Console.WriteLine("From non-admin perspective");
-//var user = new Client("Jushkin");
-//user.GetBooks();
+        //Console.WriteLine("From admin perspective");
+        //admin.GetBooks();
+        //admin.AddUser("Alan");
+        //admin.AddBook("Hello World", "Alan", "2f23f2323f23f");
+        //admin.GetBooks();
+
+        // As there is no any database to save all data,
+        // any changes made on various clients won't be saved
+        //Console.WriteLine("From non-admin perspective");
+        //var user = new Client("Jushkin");
+        //user.GetBooks();
+    }
+}
 
